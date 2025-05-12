48430991f9fe385d3805507af07fbde374cda456
martinolanza03 <lanzamartino3@gmail.com>
2025-05-12 15:59:53 +0200

    feat: create a connection with server

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,20 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+
 function HomePage() {
+
+    const [movies, setMovies] = useState([]);
+
+    function getMovies() {
+        axios.get('http://127.0.0.1:3000/movies')
+            .then(response => setMovies(response.data))
+            .catch(error => console.error('Errore:', error));
+    }
+
+    useEffect(getMovies, []);
+
+
     return <>
         <h1>Home Page</h1>
     </>
