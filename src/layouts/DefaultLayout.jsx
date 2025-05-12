d4a38f3edf8921830e51c75c1fe561bf5c6b3fc8
martinolanza03 <lanzamartino3@gmail.com>
2025-05-12 15:07:52 +0200

    feat: import header to DefaultLayout.jsx

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 
 function DefaultLayout() {
 
     return <>
+        <Header />
         <main>
             <Outlet />
         </main>
