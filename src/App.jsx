e8fac527b792c4854bd258a8c8435a9df4181899
martinolanza03 <lanzamartino3@gmail.com>
2025-05-12 15:03:45 +0200

    feat: create DefaultLayout and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout"
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
 
+          <Route Component={DefaultLayout}>
+
+          </Route>
+
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/layouts/DefaultLayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+
+function DefaultLayout() {
+
+    return <>
+        <main>
+            <Outlet />
+        </main>
+
+    </>
+}
+
+export default DefaultLayout;
